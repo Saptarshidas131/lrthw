ab5be19b7333cb388bbdba72c9032fc319e54e4e
Saptarshidas131 <saptarshidas131.gmail.com>
2021-07-09 16:57:46 +0530

    added ex3.rb

---
+++ ex3.rb
@@ -0,0 +1,36 @@
+# print the statement within quotes
+puts "I will now count my chickens:"
+
+# print Hens and evaluate the expression and prints value
+puts "Hens", 25 + 30 / 6
+# print Rooster and evaluater value of expression and prints it
+puts "Roosters", 100 - 25 * 3 % 4
+
+# print the statement within quotes
+puts "Now I will count the eggs:"
+
+# evalute the expression and prints it
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints statement within quotes
+puts "Is it true 3 + 2 < 5 - 7?"
+
+# evaluate and prints False
+puts 3 + 2 < 5 - 7
+
+# prints statement within quotes and print value of expression
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+# prints statement within quotes
+puts "Oh, that's why it's false."
+
+# prints statement within quotes
+puts "How about some more."
+
+# prints statement within quotes and evaluates and print value of expression
+puts "Is it greater?", 5 > -2
+# prints statement within quotes and evaluates and print value of expression
+puts "Is it greater or equal?", 5 >= -2
+# prints statement within quotes and evaluates and print value of expression
+puts "Is it less or equal ?", 5 <= -2
