270a3f8bea52184ecec7c008a9e74e4e700a2598
Saptarshidas131 <saptarshidas131.gmail.com>
2021-07-11 19:54:31 +0530

    added ex6.rb ex7.rb ex8.rb ex9.rb ex10.rb

---
+++ ex6.rb
@@ -0,0 +1,34 @@
+# assign x the string and replace #{10} with 10
+x = "There are #{10} types of people."
+# assign "binary" to binary
+binary = "binary"
+# assign "don't" to do_not
+do_not = "don't"
+# assign the string to y and replace binrary and do_not with its value
+y = "Those who know #{binary} and those who #{do_not}."
+
+# print x
+puts x
+# print y
+puts y
+
+# print I said then value of x
+puts "I said #{x}."
+# print I also said then the value of y
+puts "I also said: '#{y}'."
+
+# set hilarious to false
+hilarious = false
+# assign the string to joke_evaluation and replace hilarious with its value
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print the value of joke_evaluation
+puts joke_evaluation
+
+# assign w the string
+w = "This is the left side of..."
+# assign e the string
+e = "a string with a right side."
+
+# print the concatenated value of w and e
+puts w + e
---
+++ ex7.rb
@@ -0,0 +1,33 @@
+# print the statement
+puts "Mary had a little lamb."
+# print the statement
+puts "It's fleece was white as %s." % 'snow'
+# print the statement
+puts "And everywhere that Mary went."
+# print the statement
+puts "." * 10
+
+# assign the character to the vaiables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here. change it to puts
+# and see what happens.
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+puts
+puts "#{end1}#{end2}#{end3}#{end4}#{end5}#{end6}"
+puts "#{end7}#{end8}#{end9}#{end10}#{end11}#{end12}"
+
+# this just is polite use of the terminal, try removing it
+puts
