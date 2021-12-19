fb28a95a21160a269e93ffbe36fff528a8cc9d2f
saptarshi <saptarshidas131@gmail.com>
2021-12-19 21:00:35 +0530

    added ex13

--- ex13.rb
+++ ex13.rb
@@ -1,6 +1,9 @@
+# argv holds command line arguments, take whatever is in argv , unpack it and assign it to the three variables
 first, second, third = ARGV
 
+# print the script name
 puts "The script is called: #{0}"
+# print the second or the first argument after the script and so on
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
